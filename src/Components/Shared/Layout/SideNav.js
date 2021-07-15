60cac9fd292738692970eea68e1608bb611a899b
Aakash Sangwan <aakashsangwan024@gmail.com>
2021-07-15 22:40:11 +0530

    Build and deployed

--- src/Components/Shared/Layout/SideNav.js
+++ src/Components/Shared/Layout/SideNav.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './SideNav.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNewspaper, faChartBar, faCreditCard} from '@fortawesome/free-regular-svg-icons';
-import { faIgloo} from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, faChartBar, faCreditCard,faComment,faAddressCard,faCalendar} from '@fortawesome/free-regular-svg-icons';
+import { faBoxTissue, faCalculator, faDollarSign, faIgloo} from '@fortawesome/free-solid-svg-icons';
 
 
 
 const SideNav = (props)=>{
     return(
     <nav className={[styles.Nav, props.docked ? styles.Docked: styles.Undocked].join(" ")}>
-        <LogoHeader  icon={faIgloo} label="Company Name"/>
+        <LogoHeader  icon={faIgloo} label="Credflow"/>
         <ul className={styles.NavContent}>
             <NavItem  icon={faNewspaper} label="Newspaper"/>
             <NavItem  icon={faChartBar} label="Control"/>
             <NavItem  icon={faCreditCard} label="Shopping"/>
+            <NavItem  icon={faComment} label="Message"/>
+            <NavItem  icon={faAddressCard} label="Address"/>
+            <NavItem icon={faCalendar} label="Calender"/>
+            <NavItem icon={faCalculator} label="Calculator"/>
+            <NavItem icon={faBoxTissue} label="Box-Tissue"/>
+            <NavItem icon={faDollarSign} label ="Dollars"/>
         </ul>
     </nav>)
 }
